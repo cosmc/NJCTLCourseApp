53cdeeb037de2078ac7066963e1cace4a132120a
Colin McSwiggen <cmcswiggen@gmail.com>
2013-12-11 00:24:13 -0500

    Made NJCTLClass Parcelable.

--- src/org/njctl/courseapp/NJCTLClass.java
+++ src/org/njctl/courseapp/NJCTLClass.java
@@ -1,11 +1,14 @@
 package org.njctl.courseapp;
 
 import java.util.ArrayList;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by ying on 11/3/13.
  */
-public class NJCTLClass {
+public class NJCTLClass implements Parcelable {
+	
     private int classId;
     private String className;
     private ArrayList<NJCTLChapter> contents;
@@ -13,6 +16,11 @@ public class NJCTLClass {
     public NJCTLClass(String name, ArrayList<NJCTLChapter> chapters) {
         this.className = name;
         this.contents = chapters;
+    }
+    
+    // Parcelable constructor.
+    public NJCTLClass(Parcel in) {
+    	readFromParcel(in);
     }
     
     public String getName() {
@@ -26,4 +34,36 @@ public class NJCTLClass {
     public ArrayList<NJCTLChapter> getContents() {
     	return contents;
     }
+    
+    
+    // Methods for Parcelable implementation.
+    
+    @Override
+    public int describeContents() {
+    	return 0;
+    }
+    
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    	dest.writeInt(classId);
+    	dest.writeString(className);
+    	dest.writeParcelableArray((NJCTLChapter[]) contents.toArray(), flags);
+    }
+    
+    private void readFromParcel(Parcel in) {
+    	classId = in.readInt();
+    	className = in.readString();
+    	contents = new ArrayList<NJCTLChapter>();
+        in.readList(contents, NJCTLChapter.class.getClassLoader());
+    }
+    
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+    	public NJCTLClass createFromParcel(Parcel in) {
+    		return new NJCTLClass(in);
+    	}
+    	public NJCTLClass[] newArray(int size) {
+    		return new NJCTLClass[size];
+    	}
+    };
+    
 }
