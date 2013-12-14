fa8d0a621c735cbc98994b9b891d61752663e457
Colin McSwiggen <cmcswiggen@gmail.com>
2013-12-14 02:34:14 -0500

    Debugged PDF launching.

--- src/org/njctl/courseapp/DocListFragment.java
+++ src/org/njctl/courseapp/DocListFragment.java
@@ -23,7 +23,9 @@ import java.util.ArrayList;
 import java.io.FileOutputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;  
-import java.io.InputStream; 
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * Created by Colin on 12/12/13.
@@ -56,14 +58,14 @@ public class DocListFragment extends Fragment {
         		try {
         			
         			// Get an input stream for the locally stored file.
-	        		InputStream docStream = getActivity().getResources().getAssets().open( docs.get(position).getRelativePath() );
+	        		BufferedInputStream docStream = new BufferedInputStream( getActivity().getResources().getAssets().open( docs.get(position).getRelativePath() ) );
 	        		//create a buffer that has the same size as the InputStream  
-	                byte[] buffer = new byte[docStream.available()];  
+	                byte[] buffer = new byte[docStream.available()];
 	                //read the text file as a stream, into the buffer  
 	                docStream.read(buffer);
 	                //create a output stream to write the buffer into
 	                // TODO: Context.MODE_WORLD_READABLE is deprecated. Look into replacing this with a ContentProvider.
-	                BufferedOutputStream outStream = new BufferedOutputStream( getActivity().openFileOutput(docs.get(position).getTitle() + ".pdf", Context.MODE_WORLD_READABLE) );  
+	                BufferedOutputStream outStream = new BufferedOutputStream( getActivity().openFileOutput(docs.get(position).getFileName(), Context.MODE_WORLD_READABLE) );  
 	                //write this buffer to the output stream  
 	                outStream.write(buffer);  
 	                //Close the Input and Output streams  
@@ -73,16 +75,22 @@ public class DocListFragment extends Fragment {
 	                // Try to launch the PDF in a PDF viewer.
 	        		Intent intent = new Intent(Intent.ACTION_VIEW);
 	        		File docFile = new File(getActivity().getFilesDir(), docs.get(position).getFileName());
+	        		docFile.setReadable(true, false);
 	        		Uri docUri = Uri.fromFile(docFile);
 	        		intent.setDataAndType(docUri,docs.get(position).getMIMEType());
+	        		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 	        		startActivity(intent);
 	        		
-        		} catch (IOException e) {
-        			Log.w("ERROR", e.toString());
         		} catch (ActivityNotFoundException e) {
         			// Show an error message if the user does not have an appropriate application for opening the document.
         			Log.w("ERROR", e.toString());
         			Toast.makeText(getActivity(), "Error: No activity found for viewing MIME type " + docs.get(position).getMIMEType() + ".", Toast.LENGTH_SHORT).show();
+        		} catch (FileNotFoundException e) {
+        			// Show an error message if the file isn't there.
+        			Log.w("ERROR", e.toString());
+        			Toast.makeText(getActivity(), "Error: Could not open file / file not found.", Toast.LENGTH_SHORT).show();
+        		} catch (IOException e) {
+        			Log.w("ERROR", e.toString());
         		}
             }
         }); 
