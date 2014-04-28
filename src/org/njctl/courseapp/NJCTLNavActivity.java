b09dd83cb70be6d8f419619718d55cf08cb53c40
Colin McSwiggen <cmcswiggen@gmail.com>
2014-04-28 17:39:07 -0400

    Added showUnits and showTopics to NJCTLNavActivity.

--- src/org/njctl/courseapp/NJCTLNavActivity.java
+++ src/org/njctl/courseapp/NJCTLNavActivity.java
@@ -2,6 +2,7 @@ package org.njctl.courseapp;
 
 import java.util.ArrayList;
 
+import org.njctl.courseapp.model.Unit;
 import org.njctl.courseapp.model.Class;
 import org.njctl.courseapp.model.NJCTLDocList;
 import org.njctl.courseapp.model.Subject;
@@ -10,5 +11,7 @@ public interface NJCTLNavActivity {
 	public void showSubjects(ArrayList<Subject> subjects);
 	public void showClasses(ArrayList<Class> classes);
 	public void showChapters(Class theClass);
+	public void showUnits(Class theClass);
+	public void showTopics(Unit theUnit);
 	public void showDocList(NJCTLDocList docList);
 }
