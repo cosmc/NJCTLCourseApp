038219dab3d30c877eb9d5cff510615fc33a21e5
Michail Schwab <me@michaschwab.de>
2014-05-13 18:58:18 -0400

    deleted some of the subscribing classes that arent being used any more

--- src/org/njctl/courseapp/model/subscribe/ClassDownloader.java
+++
@@ -1,8 +0,0 @@
-package org.njctl.courseapp.model.subscribe;
-
-import org.njctl.courseapp.model.Class;
-
-public interface ClassDownloader
-{
-	public void downloadClass(Class theClass, DownloadFinishListener<Class> listener);
-}
