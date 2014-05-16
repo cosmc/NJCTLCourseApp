93ec4c6f01f74f8eb65408a0e8f63dc30af9e92a
Michail Schwab <me@michaschwab.de>
2014-05-16 13:09:41 -0400

    added an itemclicklistener in the unitselect for getting the clicked class.

--- src/org/njctl/courseapp/UnitSelectFragment.java
+++ src/org/njctl/courseapp/UnitSelectFragment.java
@@ -3,10 +3,14 @@ package org.njctl.courseapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.njctl.courseapp.model.Class;
 import org.njctl.courseapp.model.Unit;
@@ -27,6 +31,19 @@ public class UnitSelectFragment extends ListFragment
         UnitSelectActivity selector = (UnitSelectActivity) getActivity();
         Class theClass = selector.getNJCTLClass();
 
+        ListView lv = getListView();
+        lv.setOnItemClickListener(new OnItemClickListener()
+        {
+           @Override
+           public void onItemClick(AdapterView<?> adapter, View v, int position,
+                 long arg3) 
+           {
+                 Class theClass = (Class) adapter.getItemAtPosition(position); 
+                 Log.v("NJCTLUnitSelect", theClass.getTitle());
+                 // assuming string and if you want to get the value on click of list item
+                 // do what you intend to do on click of listview row
+           }
+        });
         
         setListAdapter(new ArrayAdapter<Unit>(getActivity(),
                 android.R.layout.simple_list_item_activated_1, theClass.getUnits()));
